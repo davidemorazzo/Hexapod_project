04ee24e1424f91803eb15350bb170d2625c48ddc
Davide Morazzo <morazzo.davide@gmail.com>
2022-05-31 11:28:59 +0200

    Arduino firwmare, ESP8266 tests

--- arduino firmware/serial_firmware/serial_firmware.ino
+++ arduino firmware/serial_firmware/serial_firmware.ino
@@ -1,8 +1,30 @@
 #include <Servo.h>
+#define TRIG_PIN A2 
+#define ECHO_PIN A3
 
 // Servomotors declaration
 Servo servomotors[12];
 Servo head_servo;
+
+void ESP8266_ATCOMMAND(){
+   Serial.begin(115200);       // set up a wifi serial communication baud rate 115200
+//   pinMode(controller, OUTPUT);    // sets the RelayPin to be an output
+   Serial.println("AT+CWMODE=3\r\n");//set to softAP+station mode
+   delay(2000);     //delay 4s
+   
+   Serial.println("AT+CWSAP=\"Adeept_ESP\",\"12345678\",8,2\r\n");   //TCP Protocol, server IP addr, port
+    delay(2000);     //delay 4s
+   Serial.println("AT+RST\r\n");     //reset wifi
+   delay(2000);     //delay 4s
+
+   Serial.println("AT+CIPMUX=1\r\n");
+   delay(2000);
+   Serial.println("AT+CIPSERVER=1,8080\r\n");
+   delay(2000);
+   Serial.println("AT+CIPSTO=7000\r\n");
+   delay(2000);
+}
+
 
 void setup() {
   // Attach servomotors to the correct pin
@@ -12,7 +34,9 @@ void setup() {
   servomotors[3].attach(4); //s22
   servomotors[4].attach(7); //s31
   servomotors[5].attach(6); //s32
-  servomotors[6].attach(13); //s41
+  //servomotors[6].attach(13); //s41
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
   servomotors[7].attach(12); //s42
   servomotors[8].attach(11); //s51
   servomotors[9].attach(10); //s52
@@ -30,9 +54,14 @@ void setup() {
   0x07      1024      30.64
   */
   TCCR2B = TCCR2B & 0b11111000 | 0x05;
+
+  /* Setup Ultrasonic sensor HC-SR04 */
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
   
   // begin serial
-  Serial.begin(57600);
+  Serial.begin(115200);
+  ESP8266_ATCOMMAND();
   // wait serial to connect
   Serial.flush();
   while(!Serial){}
@@ -43,6 +72,8 @@ uint8_t command_id;
 char bytes_read[1];
 uint8_t current_angle;
 float dc;
+long duration;
+int distance;
 
 
 void loop() {
@@ -56,7 +87,7 @@ void loop() {
   
   if(Serial.available() > 0){
     // something to read
-
+    digitalWrite(13, HIGH);
     command_id = Serial.read();  
     //Serial.write(command_id);
     
@@ -64,16 +95,28 @@ void loop() {
     // commands options
     switch(int(command_id)){
       case 1:
+        
         // read motor positions
         for(int8_t i=0; i<12; i++){
-          Serial.readBytes(bytes_read, 1);
-          current_angle = (uint8_t) bytes_read[0];
+          current_angle = uint8_t(Serial.read());
+          //current_angle = (uint8_t) bytes_read[0];
           servomotors[i].write((int)current_angle);
         }
       break;
       
       case 2:
-      // write ultrasonic measurement
+        // write ultrasonic measurement
+        digitalWrite(TRIG_PIN, LOW);
+        delayMicroseconds(2);
+        digitalWrite(TRIG_PIN, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(TRIG_PIN, LOW);
+        duration = pulseIn(ECHO_PIN, HIGH);
+        distance = duration * 0.034/2;
+
+        // Write on Serial port
+        //Serial.write()
+        
       break;
 
       case 3:
